0acca1bb571b39224ecf0b01bfd2afdd857907b9
Your Name <ekka.beaula@gmail.com>
2024-05-04 23:07:50 +0200

    Uploadthing added upload button

--- src/app/components/TopNav.tsx
+++ src/app/components/TopNav.tsx
@@ -1,18 +1,31 @@
+"use client";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "~/utils/uploadthing";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 export const TopNav = () => {
+  const router = useRouter();
+
   return (
     <div>
       {" "}
       <nav className="flex w-full items-center justify-between  border   p-4 text-xl font-semibold shadow-md ">
         <div className="">Gallery</div>
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
+        <div className="flex flex-row">
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UploadButton
+              endpoint="imageUploader"
+              onClientUploadComplete={() => {
+                router.refresh();
+              }}
+            />
+            <UserButton />
+          </SignedIn>
+        </div>
       </nav>
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,10 @@ import {
 } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import { TopNav } from "./components/TopNav";
+import "@uploadthing/react/styles.css";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -28,6 +32,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
         <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
           <TopNav />
           {children}
